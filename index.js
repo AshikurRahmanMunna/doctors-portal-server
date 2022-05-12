d92c6c55cd2959d1f916197e04f8fc79e252da44
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-05-12 21:52:03 +0600

    MongoDB Connection Done

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.get('/', (req, res) => {
   res.send('Hello From Doctors Portal')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1cmm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database Connected');
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Doctors Portal Is Running on port ${port}`)
 })
